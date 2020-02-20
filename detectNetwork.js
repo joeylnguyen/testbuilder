d9e07c9f6bea8724e65b1eb74e0960ae6ccb3730
Joey Nguyen <joeylnguyen95@gmail.com>
2020-02-20 03:14:12 -0800

    Replace object with conditional statements
    Removed the object that stored the network cardNumber lengths as properties. Instead, I used conditional statements that evaluates length of the cardNumber and then returns the correct network name. If there's a case where there are two networks that have the same number lengths, I then evaluate the first digit of their prefixes.

--- detectNetwork.js
+++ detectNetwork.js
@@ -7,26 +7,45 @@
 //   1. The first few numbers (called the prefix)
 //   2. The number of digits in the number (called the length)
 
-var detectNetwork = function(cardNumber) {
-  // Note: `cardNumber` will always be a string
+ // Note: `cardNumber` will always be a string
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
   // The American Express network always starts with a 34 or 37 and is 15 digits long
+  // Visa always has a prefix of 4 and a length of 13, 16, or 19.
+  // MasterCard always has a prefix of 51, 52, 53, 54, or 55 and a length of 16.
 
-  // Once you've read this, go ahead and try to implement this function, then return to the console.
+ // create length var for convenience
+ // create firstPrefix var for convenience
+ // If cardNumber.length is 14
+   // return "Diner's Club"
+ // If cardNumber.length is 15
+   // return "American Express"
+ // If cardNumber.length is 13
+   // return "Visa"
+ // If cardNumber.length is 19
+   // return "Visa"
+ // If cardNumber.length is 16
+   // If the first number is 4
+     // return "Visa"
+     // Otherwise return "MasterCard"
 
-  // Detect length of string
-  // Return network based on length
+var detectNetwork = function(cardNumber) {
+  var length = cardNumber.length;
+  var firstPrefix = parseInt(cardNumber[0]);
 
-  // Create object that has properties for cardNumber length
-  // Return value at property length key
+  if (length === 14) {
+  	return "Diner's Club";
+  } else if (length === 15) {
+  	return "American Express";
+  } else if (length === 13 || length === 19) {
+  	return "Visa";
+  } else if (length === 16) {
+  	if (firstPrefix === 4) {
+  		return "Visa";
+  	} else {
+  		return "MasterCard";
+  	}
+  }
+}
 
-  var networkData = {
-  	14 : "Diner's Club",
-  	15 : "American Express"
-  };
-
-  return networkData[cardNumber.length];
-
-};
 
 
